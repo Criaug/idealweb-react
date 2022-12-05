a0d4a55a6c57108557cd8ad6117cf33cf67dd77e
Criaug <cristianaugustolopes@hotmail.com>
2022-12-05 17:54:40 -0300

    05122022

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,64 @@
-import React, { ChangeEvent, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { AuthContext } from '../../contexts/Auth/AuthContext';
 import styles from './index.module.css';
 
 const Login = () => {
-  const [inputs, setInputs] = useState({
+  const [input, setInput] = useState({
     username: '',
     password: '',
   });
-
-  const campoFocus = useRef(null);
-
-  const validaLogin = () => {};
-  const handleChange = (campo: string, texto: string) => {};
-  const handleCursor = (e: React.FormEvent<EventTarget>) => {};
-
+  const campoFocus = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<boolean>(false);
   //controle teclado virtual
-  const [campo, setCampo] = useState<string>('username');
+  const [campo, setCampo] = useState('username');
   const [cursor, setCursor] = useState<number>(0);
+  const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    campoFocus?.current?.focus();
+  }, []);
+
+  const validaLogin = (e: any) => {
+    if (e) e.preventDefault();
+    setError(false);
+
+    if (input.username && input.password) {
+      const autentica = await auth.login(input.username, input.senha);
+    }
+  };
+
+  const handleChange = (campo: string, texto: string) => {
+    setInput((prevState) => ({ ...prevState, [campo]: texto }));
+  };
+
+  const handleCursor = (e: any) => {
+    setCursor(e.target.selectionStart);
+  };
+
+  const eventoTecla = (tecla: string) => {
+    if (campo !== null) {
+      if (tecla === 'DEL') {
+        setInput((prevState) => ({
+          ...prevState,
+          [campo]:
+            input[campo].substring(0, cursor - 1) +
+            input[campo].substring(cursor),
+        }));
+        setCursor(Number(cursor) - 1);
+      } else if (tecla === 'ENTER') {
+        validaLogin(false);
+      } else {
+        setInput((prevState) => ({
+          ...prevState,
+          [campo]:
+            input[campo].substring(0, cursor) +
+            tecla +
+            input[campo].substring(cursor),
+        }));
+        setCursor(Number(cursor) + 1);
+      }
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -34,7 +77,7 @@ const Login = () => {
           onMouseUp={(e) => {
             handleCursor(e);
           }}
-          value={inputs.username}
+          value={input.username}
         ></input>
 
         <input
@@ -51,7 +94,7 @@ const Login = () => {
           onMouseUp={(e) => {
             handleCursor(e);
           }}
-          value={inputs.password}
+          value={input.password}
         ></input>
         <button id="login" name="login" className={styles.button}>
           Sign In
